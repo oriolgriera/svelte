4f8a0154759efd838b7780303763a7cb5912db91
oriolgriera <oriolgrierajimenez@gmail.com>
2021-10-13 16:06:39 +0200

    Upload svelte-app

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Welcome to {name} App!</h1>
+	<p>This is my first app with Svelte.</p>
+</main>
+
+<style>
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
